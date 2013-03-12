76a7b84c631127b5230b86759974da13d47b3612
Alla Gofman <att.lattice@gmail.com>
2013-03-12 20:17:12 +0200

    Add new SpesialKeys to WindowPlacements.SpecialKeys enum
    To support more shortcuts

--- src/TestStack.White/WindowsAPI/WindowPlacement.cs
+++ src/TestStack.White/WindowsAPI/WindowPlacement.cs
@@ -91,24 +91,70 @@ namespace White.Core.WindowsAPI
         public enum SpecialKeys
         {
             //http://pinvoke.net/default.aspx/user32/SendInput.html, http://delphi.about.com/od/objectpascalide/l/blvkc.htm
+            
+            LBUTTON = 0x01, //Left mouse button
+            RBUTTON = 0x02, //Right mouse button
+            CANCEL = 0x03, //Control-break processing
+            MBUTTON = 0x04, //Middle mouse button (three-button mouse)
+            BACKSPACE = 0x08, //BACKSPACE key
+            TAB = 0x09, //TAB key
+            CLEAR = 0x0C, //CLEAR key
+            RETURN = 0x0D, //ENTER key
             SHIFT = 0x10,
             CONTROL = 0x11,
             ALT = 0x12,
             LEFT_ALT = 0xA4,
             RIGHT_ALT = 0xA5,
-            RETURN = 0x0D,
-            RIGHT = 0x27,
-            BACKSPACE = 0x08,
-            LEFT = 0x25,
             ESCAPE = 0x1B,
-            TAB = 0x09,
+            SPACE = 0x20, //SPACEBAR
+            PAGEUP = 0x21,//PAGE UP key (PRIOR)
+            PAGEDOWN = 0x22, //(NEXT)
+            END = 0x23,  
             HOME = 0x24,
-            END = 0x23,
+            LEFT = 0x25,//LEFT ARROW key
             UP = 0x26,
+            RIGHT = 0x27,//RIGHT ARROW key
             DOWN = 0x28,
+            SELECT = 0x29, //SELECT key           
+            PRINT = 0x2A, 
+            EXECUTE = 0x2B,
+            PRINTSCREEN = 0x2C, //PRINT SCREEN key (SNAPSHOT)
             INSERT = 0x2D,
             DELETE = 0x2E,
+            HELP = 0x2F,
             CAPS = 0x14,
+            
+            A = 0x41,
+            B = 0x42,
+            C = 0x43,
+            D = 0x44,
+            E = 0x45,
+            F = 0x46,
+            G = 0x47,
+            H = 0x48,
+            I = 0x49,
+            J = 0x4A,
+            K = 0x4B,
+            L = 0x4C,
+            M = 0x4D,
+            N = 0x4E,
+            O = 0x4F,
+            P = 0x50,
+            Q = 0x51,
+            R = 0x52,
+            S = 0x53,
+            T = 0x54,
+            U = 0x55,
+            V = 0x56,
+            W = 0x57,
+            X = 0x58,
+            Y = 0x59,
+            Z = 0x5A,
+
+            LWIN = 0x5B,
+            RWIN = 0x5C,
+            APPS = 0x5D,
+
             F1 = 0x70,
             F2 = 0x71,
             F3 = 0x72,
@@ -133,41 +179,17 @@ namespace White.Core.WindowsAPI
             F22 = 0x85,
             F23 = 0x86,
             F24 = 0x87,
-            PAGEUP = 0x21,
-            PAGEDOWN = 0x22,
-            PRINT = 0x2A,
-            PRINTSCREEN = 0x2C,
-            SPACE = 0x20,
+
             NUMLOCK = 0x90,
-            SCROLL = 0x91,
-            LWIN = 0x5B,
-            RWIN = 0x5C,
-            APPS = 0x5D,
-            A = 0x41,
-            B = 0x42,
-            C = 0x43,
-            D = 0x44,
-            E = 0x45,
-            F = 0x46,
-            G = 0x47,
-            H = 0x48,
-            I = 0x49,
-            J = 0x4A,
-            L = 0x4C,
-            M = 0x4D,
-            N = 0x4E,
-            O = 0x4F,
-            P = 0x50,
-            Q = 0x51,
-            R = 0x52,
-            S = 0x53,
-            T = 0x54,
-            U = 0x55,
-            V = 0x56,
-            W = 0x57,
-            X = 0x58,
-            Y = 0x59,
-            Z = 0x5A
+            SCROLL = 0x91,            
+            LSHIFT = 0xA0, //Left SHIFT key
+            RSHIFT = 0xA1, //Right SHIFT key
+            LCONTROL = 0xA2, //Left CONTROL key
+            RCONTROL = 0xA3, //Right CONTROL key
+            LMENU = 0xA4, //Left MENU key
+            PLAY = 0xFA, //Play key
+            ZOOM = 0xFB //Zoom key
+
         }
     }
 
